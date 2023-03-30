f705629f43abe224a3e40eb38433da3428c37699
Matteo Digiorgio <matthew.digiorgio@gmail.com>
2023-03-30 18:44:22 +0200

    Wrote the contract

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -2,5 +2,64 @@
 pragma solidity ^0.8.9;
 
 contract BuyMeACoffee {
+    // Event to emit when Memo i created
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
 
+    // Memo struct
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    // List of all memos received from friends.
+    Memo[] memos;
+
+    //  Address of contract deployer
+    address payable owner;
+
+    // Deploy logic
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev buy a coffee for contract owner
+     * @param _name name of the cofee buyer
+     * @param _message a nice message from the coffee buyer
+     */
+    function buyCoffee(string memory _name, string memory _message) public payable {
+        require(msg.value > 0, "Can't buy coffee with 0 eth");
+
+        // Add the memo to storage!
+        memos.push(Memo(msg.sender, block.timestamp, _name, _message));
+
+        // Emit a log event when a new memo is created!
+        emit NewMemo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        );
+    }
+
+    /**
+     * @dev send the entire balance stored in this contrct to the owner
+     */
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+
+    /**
+     * @dev retrive all the memos received and stored on the blockchain
+     */
+    function getMemos() public view returns(Memo[] memory) {
+        return memos;
+    }
 }
